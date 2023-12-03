3d3305b86597dfd341de35b5d3520dd3791b1388
mesoud5 <mesoudtemam5@gmail.com>
2023-12-03 03:07:38 -0700

    S

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,7 +1,8 @@
-word = "Holberton"
-word_first_3 = "hol"
-word_last_2 = "on"
-middle_word = "olberto"
-print("First 3 letters: {}".format(word_first_3))
-print("Last 2 letters: {}".format(word_last_2))
-print("Middle word: {}".format(middle_word))
+Word = "Holberton"
+Word_first_3 = "hol" 
+Word_last_2 = "hol"
+Middle_word = "hol"
+
+print("First 3 letters: {}".format(Word_first_3))
+print("Last 2 letters: {}".format(Word_last_2))
+print("Middle word: {}".format(Middle_word))
