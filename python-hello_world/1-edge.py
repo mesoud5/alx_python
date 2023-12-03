958019ea2ba1a84efc3537e46b241ff537ce57cc
mesoud5 <mesoudtemam5@gmail.com>
2023-12-03 01:47:42 -0700

    task-2

---
+++ python-hello_world/1-edge.py
@@ -0,0 +1,9 @@
+
+word = "holberton"
+word_first_3, *middle_word, word_last_2 = word
+word_first_3 = "".join(word_first_3)
+word_last_2 = "".join(word_last_2)
+middle_word = "".join(middle_word)
+print("word_first_3:", word_first_3)
+print("middle_word:", middle_word)
+print("word_last_2:", word_last_2)
