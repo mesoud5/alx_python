6a763151858e439b8f1b982c38a1b54d41e62c2b
mesoud5 <mesoudtemam5@gmail.com>
2023-12-03 05:03:52 -0700

    s

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -6,8 +6,11 @@ number = random.randint(-10000, 10000)
 # Extract the last digit of the number
 last_digit = abs(number) % 10
 
+# Determine the sign for display
+sign = '-' if number < 0 else ''
+
 # Output the result based on conditions
-print(f"Last digit of {number} is {last_digit} and", end=' ')
+print(f"Last digit of {number} is {sign}{last_digit} and", end=' ')
 
 if last_digit > 5:
     print("is greater than 5")
