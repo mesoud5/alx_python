89645574ea91997635a1074877da4271514ac7a0
mesoud5 <mesoudtemam5@gmail.com>
2023-12-07 13:07:23 -0700

    1-power.py

---
+++ python-functions/1-power.py
@@ -0,0 +1,13 @@
+def pow(a, b):
+    return a ** b
+
+result = pow(2, 2)
+print(result)
+result = pow(98, 2)
+print(result)
+result = pow(98, 0)
+print(result)
+result = pow(100, -2)
+print(result)
+result = pow(-4, 5)
+print(result)
