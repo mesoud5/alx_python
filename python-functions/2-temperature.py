a3a04c157b58a4a29ea232dbd1c997f00e2b3769
mesoud5 <mesoudtemam5@gmail.com>
2023-12-07 13:21:24 -0700

    2-temprature.py

---
+++ python-functions/2-temperature.py
@@ -0,0 +1,7 @@
+def convert_to_celsius(fahrenheit):
+    celsius = (fahrenheit -32) *5/9
+    return round(celsius, 2)
+print(convert_to_celsius(100))
+print(convert_to_celsius(-40))
+print(convert_to_celsius(-459.67))
+print(convert_to_celsius(32))
