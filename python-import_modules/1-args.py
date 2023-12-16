e0272bfaf3608ee1b3a7918b0fe53608a5709109
mesoud5 <mesoudtemam5@gmail.com>
2023-12-16 13:31:17 -0700

    task 1

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,11 +1,21 @@
 import sys
+
+def print_arguments():
+    # Get the total number of arguments
+    num_arguments = len(sys.argv) - 1  # The first argument is the script name
+
+    # Get the list of arguments
+    arguments_list = sys.argv[1:]
+
+    # Print the results
+    print(f"{num_arguments} argument{'' if num_arguments == 1 else 's'}:")
+    for i, arg in enumerate(arguments_list, start=1):
+        print(f"{i}: {arg}")
+    
+    # Calculate the length of the first argument
+    first_arg_length = len(arguments_list[0]) if num_arguments >= 1 else 0
+
+    print(f"\n({first_arg_length} )")
+
 if __name__ == "__main__":
-    argc = len(sys.argv) - 1
-    plural_suffix = 's' if argc != 1 else ''
-    print(f"{argc} argument{plural_suffix}:", end='\n')
-    if argc == 0:
-        print('.')
-    else:
-        print()
-        for i in range(1, argc + 1):
-            print(f"{i}: {sys.argv[i]}")
+    print_arguments()
