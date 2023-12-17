2fee4d414a3da74a20a8b102591204b8fbe2ea14
mesoud5 <mesoudtemam5@gmail.com>
2023-12-17 05:18:59 -0700

    d

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,6 +1,7 @@
 def raise_exception_msg(message=""):
     raise NameError(message)
-try:
-    raise_exception_msg("c is fun")
-except NameError as ne:
-    print(f"{ne}")
+if __name__ == "__main__":
+    try:
+        raise_exception_msg("c is fun")
+    except NameError as ne:
+        print(ne)
