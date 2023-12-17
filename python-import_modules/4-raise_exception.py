fd26c7bcdb85c6c7cd48398819a4c38893f7de41
mesoud5 <mesoudtemam5@gmail.com>
2023-12-17 04:38:47 -0700

    excpetion raised

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -1,5 +1,7 @@
-x = -1
-
-if x < 0:
-    raise Exception("sorry,mesoud")
-
+def raise_exception():
+    x = 1 + '1'
+if __name__ == "__main__":
+    try:
+        raise_exception()
+    except TypeError:
+        print("exception raised")    
