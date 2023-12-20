ac46f246c20239ad8908e435c95b0292914412bc
mesoud5 <mesoudtemam5@gmail.com>
2023-12-20 05:59:04 -0700

    task 1

---
+++ python-data_structures/1-print_matrix_integer.py
@@ -0,0 +1,12 @@
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for element in row:
+            print(f"{element}", end=" ")
+        print()
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]           
+print_matrix_integer(matrix)
+print("--")
