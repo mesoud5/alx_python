0d5396fcdbb6775bb5b8ff05a2877f63b09e8533
mesoud5 <mesoudtemam5@gmail.com>
2023-12-20 08:55:00 -0700

    ghjn

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,11 +1,7 @@
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            if j < len(matrix[i]) - 1:
-                print("{:d}".format(matrix[i][j]), end=" ")
-            else:
-                print("{:d}".format(matrix[i][j]), end="")
-        print("")
+    for row in matrix:
+        print(" ".join("{:d}".format(num) for num in row))
+    
 
 matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 print_matrix_integer(matrix)
