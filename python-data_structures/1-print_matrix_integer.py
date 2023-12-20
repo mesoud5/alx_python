5503a5115f7d4118d622a11b145cfa739a328b65
mesoud5 <mesoudtemam5@gmail.com>
2023-12-20 08:14:36 -0700

    bb

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,14 +1,16 @@
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            if j < len(matrix[i]) - 1:
-                print("{:d}".format(matrix[i][j]), end = " ")
-            else:
-                print("{:d}".format(matrix[1][j]), end = "")  
-                print("")  
-matrix = [
+             if j < len(matrix[i]) - 1:
+                 print("{:d}".format(matrix[i][j]), end=" ")
+             else:
+                 print("{:d}".format(matrix[i][j]), end="")
+        print("")     
+# Example usage:
+matrix_example = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
-]                
-print_matrix_integer(matrix)
+]
+
+print_matrix_integer(matrix_example)
