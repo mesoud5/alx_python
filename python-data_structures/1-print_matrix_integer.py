f388152cd9f69dc1c74fcd3c621242c72bdbe9f3
mesoud5 <mesoudtemam5@gmail.com>
2023-12-20 08:21:01 -0700

    o

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,12 +1,15 @@
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-             if j < len(matrix[i]) - 1:
-                 print("{:d}".format(matrix[i][j]), end=" ")
-             else:
-                 print("{:d}".format(matrix[i][j]), end="")
-        if i < len(matrix) - 1:         
-            print("")     
+            if j < len(matrix[i]) - 1:
+                print("{:d}".format(matrix[i][j]), end=" ")
+            else:
+                print("{:d}".format(matrix[i][j]), end="")
+        
+        # Check if it's not the last row to avoid an extra newline
+        if i < len(matrix) - 1:
+            print("")
+
 # Example usage:
 matrix_example = [
     [1, 2, 3],
