5ccec65051d306c5e95809633f1bf7c7aa3bb8de
mesoud5 <mesoudtemam5@gmail.com>
2023-12-20 08:36:16 -0700

    ujtd

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,21 +1,16 @@
 def print_matrix_integer(matrix=[[]]):
-    #print("Matrix Dimensions:", len(matrix), "x", len(matrix[0]))
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             if j < len(matrix[i]) - 1:
                 print("{:d}".format(matrix[i][j]), end=" ")
             else:
                 print("{:d}".format(matrix[i][j]), end="")
-        
-        if i < len(matrix) - 1:
-            print("")
-
-# Example usage:
-matrix_example = [
+        print("")
+matrix = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
 ]
 
-print_matrix_integer(matrix_example)
+print_matrix_integer(matrix)
 
