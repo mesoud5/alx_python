5fed616b188efea1e07b9b1fc705c25cd0090c85
mesoud5 <mesoudtemam5@gmail.com>
2023-12-20 08:41:03 -0700

    hbi

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -6,11 +6,7 @@ def print_matrix_integer(matrix=[[]]):
             else:
                 print("{:d}".format(matrix[i][j]), end="")
         print("")
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
 
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 print_matrix_integer(matrix)
 
