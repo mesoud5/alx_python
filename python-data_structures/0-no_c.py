2a67ecdac78ce758a6e6782c5acecb870adf94a8
mesoud5 <mesoudtemam5@gmail.com>
2023-12-20 05:24:44 -0700

    correction

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,11 +1,14 @@
 def no_c(my_string):
     result_string = ''
     for char in my_string:
-        if char.lower() not in ['c', 'C']:
+      
+        if char.lower() != 'c':
+            
             result_string += char
+
     return result_string
 
 
-print(no_c("holberton school"))
+print(no_c("Holberton School"))
 print(no_c("Chicago"))
 print(no_c("C is fun!"))
