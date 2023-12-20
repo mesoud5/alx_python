f41a91ca80b1985dda73aa9b394a380c978fae6e
mesoud5 <mesoudtemam5@gmail.com>
2023-12-20 05:26:40 -0700

    a

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,14 +1,15 @@
 def no_c(my_string):
     result_string = ''
     for char in my_string:
-      
-        if char.lower() != 'c':
-            
+        # Check if the lowercase version of the current character is not 'c'
+        # and the original character is not 'C'
+        if char.lower() != 'c' and char != 'C':
+            # Append the character to the result_string
             result_string += char
 
     return result_string
 
-
+# Example usage:
 print(no_c("Holberton School"))
 print(no_c("Chicago"))
 print(no_c("C is fun!"))
