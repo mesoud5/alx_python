0a634afc898dc6b5628cbdad5d72966df0dac6ed
mesoud5 <mesoudtemam5@gmail.com>
2023-12-21 01:50:34 -0700

    commit

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -1,9 +1,4 @@
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        return  None
-    else:
-        return len(sentence), sentence[0]
-sentence = "At holberton school, i learn't c"
-multiple_returns(sentence)
-result = multiple_returns(sentence)
-print(result)
+        return 0, None
+    return len(sentence), sentence[0]
