9fcb851ddd13f6c9ffe20a6f358b948e7c900b45
mesoud5 <mesoudtemam5@gmail.com>
2023-12-21 00:55:01 -0700

    task 0

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,7 +1,6 @@
 def no_c(my_string):
     return ''.join(char for char in my_string if char not in 'cC')
 
-
-print(no_c("holberton school"))
+print(no_c("Holberton School"))
 print(no_c("Chicago"))
 print(no_c("C is fun!"))
