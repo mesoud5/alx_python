527fc47520b4bff7f190d66da48949685106ca37
mesoud5 <mesoudtemam5@gmail.com>
2023-12-21 01:26:03 -0700

    task 1

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,3 +1,12 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         print(" ".join("{:d}".format(num) for num in row))
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
