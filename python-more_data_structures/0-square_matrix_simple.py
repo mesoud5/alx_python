8419f100c8a357af4b0d0f26f3a4982a5a228d3b
mesoud5 <mesoudtemam5@gmail.com>
2023-12-26 05:34:00 -0700

    correction

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,16 +1,40 @@
-def square_matrtix_simple(matrix=[]):
+def square_matrix_simple(matrix=[]):
+    """
+    Computes the square value of all integers in a matrix.
+
+    Parameters:
+    - matrix (list of lists): The input matrix containing integers.
+
+    Returns:
+    - list of lists: A new matrix with each integer squared.
+    """
+    # Create a new matrix with the same size as the input matrix
     new_matrix = []
+
+    # Iterate over rows in the input matrix
     for row in matrix:
+        # Create a new row for the squared values
         squared_row = []
+        
+        # Iterate over elements in the current row and square each element
         for element in row:
-            squared_row.append(element**2)
+            squared_row.append(element ** 2)
+        
+        # Add the squared row to the new matrix
         new_matrix.append(squared_row)
+
     return new_matrix
-matrix = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9]
-]
-new_matrix = square_matrtix_simple(matrix)
-print(new_matrix)
-print(matrix)
+
+# Example usage
+if __name__ == "__main__":
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+
+    new_matrix = square_matrix_simple(matrix)
+
+    # Display the result
+    print(new_matrix)
+    print(matrix)
