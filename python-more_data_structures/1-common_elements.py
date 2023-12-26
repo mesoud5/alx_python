718b99089dbb5139260b98c80485ea43f1767e03
mesoud5 <mesoudtemam5@gmail.com>
2023-12-26 06:05:57 -0700

    c

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -1,7 +1,8 @@
 def common_elements(set_1, set_2):
-    return set_1 & set_2 
-set_1 = { "python", "c", "javascript" }
-set_2 = { "Bash", "c", "Ruby", "Perl" }
+    return set_1 & set_2
+if __name__ == "__main__":
+    set_1 = { "python", "c", "javascript" }
+    set_2 = { "Bash", "c", "Ruby", "Perl" }
 
 c_set = common_elements(set_1, set_2)
 print(sorted(list(c_set)))
