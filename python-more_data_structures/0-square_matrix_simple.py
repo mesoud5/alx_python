fc799e50a9239b5be705a58ff61d6e9c8f7526e0
mesoud5 <mesoudtemam5@gmail.com>
2023-12-26 05:19:38 -0700

    mesoud

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,8 @@
 def square_matrtix_simple(matrix=[]):
     squared_matrtix = [[x**2 for x in row]for row in matrix]
     return squared_matrtix
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+new_matrix = square_matrtix_simple(matrix)
+print(new_matrix)
+print(matrix)
