2b716f5f8fc75ebfc5dcf489738d24074e1ad206
mesoud5 <mesoudtemam5@gmail.com>
2023-12-26 06:29:55 -0700

    task2

---
+++ python-more_data_structures/2-update_dictionary.py
@@ -0,0 +1,7 @@
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return a_dictionary
+my_dict = {'name':'mesoud', 'age':19, 'citizen':'ethiopian'}
+my_dict['age'] = 30
+my_dict['major'] = 'software engineer'
+print(my_dict)
