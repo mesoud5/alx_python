79c161782218dcce151eff693755294a7092ffba
mesoud5 <mesoudtemam5@gmail.com>
2023-12-26 07:39:59 -0700

    task 2

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,7 +1,22 @@
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
     return a_dictionary
-my_dict = {'name':'mesoud', 'age':19, 'citizen':'ethiopian'}
-my_dict['age'] = 30
-my_dict['major'] = 'software engineer'
-print(my_dict)
+
+if __name__ == "__main__":
+    def print_sorted_dictionary(my_dict):
+        keys = sorted(my_dict.keys())
+        for k in keys:
+            print("{}: {}".format(k, my_dict[k]))
+
+    a_dictionary = {'language': "C", 'number': 89, 'track': "Low level"}
+    new_dict = update_dictionary(a_dictionary, 'language', "Python")
+    print_sorted_dictionary(new_dict)
+    print("--")
+    print_sorted_dictionary(a_dictionary)
+    print("--")
+    print("--")
+    new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
+    print_sorted_dictionary(new_dict)
+    print("--")
+    print_sorted_dictionary(a_dictionary)
+
