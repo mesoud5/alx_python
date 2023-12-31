49b38b973c28d85182b41fb6a69442bfc2f49731
mesoud5 <mesoudtemam5@gmail.com>
2023-12-31 01:07:25 -0700

    w

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -4,7 +4,6 @@ Write a class Square that defines a square by:
 Private instance attribute: size
 Instantiation with size (no type/value verification)
 You are not allowed to import any module
-
 """
 
 class Square:
