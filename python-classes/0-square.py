85deea9ba7e27fcca9f00de6e995b6cadb9dac15
mesoud5 <mesoudtemam5@gmail.com>
2023-12-31 01:06:06 -0700

    task 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,12 @@
+"""
+Write a class Square that defines a square by:
+
+Private instance attribute: size
+Instantiation with size (no type/value verification)
+You are not allowed to import any module
+
+"""
+
 class Square:
     """
     This class defines a square.
