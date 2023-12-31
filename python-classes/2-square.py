b93f407c1a19a250e8565c766905a4e02d15d3aa
mesoud5 <mesoudtemam5@gmail.com>
2023-12-31 04:09:17 -0700

    task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -3,34 +3,33 @@ we will Write a class Square that defines a square by:
 
 (based on 1-square.py)
 """
-
-class square:
+class Square:
     """
-     class Square that defines a square by: (based on 1-square.py)
+    Class Square that defines a square by:
 
-         Private instance attribute: size
-         Instantiation with optional size: def __init__(self, size=0):
-         size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
-         if size is less than 0, raise a ValueError exception with the message size must be >= 0
-         Public instance method: def area(self): that returns the current square area
-         with out importing any module 
+    - Private instance attribute: size
+    - Instantiation with optional size: def __init__(self, size=0)
+    - size must be an integer, otherwise raise a TypeError exception with the message "size must be an integer"
+    - if size is less than 0, raise a ValueError exception with the message "size must be >= 0"
+    - Public instance method: def area(self) that returns the current square area
     """
+
     def __init__(self, size=0):
         """
-        initializes a new instances of the square class
-        parameters:
-        size which set to 0 by defalut
+        Initializes a new instance of the Square class.
+
+        Parameters:
+        - size (int, optional): The size of the square is defaulted to 0 if not provided.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueError("size must be >=0")
+            raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         """
-        this is a public instance method that returns 
-          
-         -the current square area
-        
+        Public instance method that returns the current square area.
         """
-        return self.__size    
+        return self.__size
+
