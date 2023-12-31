35a4d5bb92ffbd06691ecd7bbed318e23acc2454
mesoud5 <mesoudtemam5@gmail.com>
2023-12-31 00:49:17 -0700

    task 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,8 @@
+"""This module creates class square with
+    -private instance attribute 
+    -Instantiation with size (no type/value verification)
+    -with oout importing any module
+    """
 class square:
     def __init__(self, size):
         self.__size = size
