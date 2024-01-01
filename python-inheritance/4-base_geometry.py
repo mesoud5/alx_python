f96fb1dfff37008aea55c560c29c4e6f78e8d14b
mesoud5 <mesoudtemam5@gmail.com>
2024-01-01 12:05:39 -0700

    bcx

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -7,4 +7,7 @@ class BaseGeometry:
     This is an empty class that serves as a base for geometry-related classes.
     """
     def area(self):
+        """
+        This is a method that raises an exception
+        """
         raise BaseGeometry("area() is not implemented")
