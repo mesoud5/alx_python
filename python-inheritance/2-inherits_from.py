fd62ba68c4bea15fa3977d4f5cddcc9230d06d89
mesoud5 <mesoudtemam5@gmail.com>
2024-01-01 11:38:30 -0700

    task 2

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,4 +1,13 @@
+"""
+in this module we will Write a function that returns True if the object is an instance of a class that inherited (directly or indirectly) 
+from the specified class ; otherwise False.
+
+"""
 def inheritance_from(obj, a_class):
+    """
+    this function returns True if the object is an instance of a class that inherited (directly or indirectly) 
+    from the specified class ; otherwise False.
+    """
     if issubclass(type(obj), a_class):
         return True
     for base_class in type(obj).__base__:
