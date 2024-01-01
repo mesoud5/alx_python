4ef08a3199947afb6179d5d31bb8036e03202a91
mesoud5 <mesoudtemam5@gmail.com>
2024-01-01 04:42:27 -0700

    task 0

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,6 +1,13 @@
 class a_class:
     pass
-def is_same_class(obj, a_class):
-    return type(obj) == a_class
+
+def is_same_class(obj, specified_class):
+    return type(obj) == specified_class
+
+# Creating an instance of 'a_class'
 obj = a_class()
-print(is_same_class(obj, a_class))
+
+# Checking if 'obj' is exactly an instance of 'a_class' using 'is_same_class'
+result = is_same_class(obj, a_class)
+print(result)  # Output: True
+
