cb3f0bb35a631d6dfe9d481b7a4fa2f53ff39152
mesoud5 <mesoudtemam5@gmail.com>
2024-01-04 05:11:31 -0700

    task

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,6 +1,15 @@
 class Base:
     """
-    Base class for managing id attribute in all other classes.
+     Base class for managing id attribute in all other classes.
+
+    Attributes:
+        __nb_objects (int): A private class attribute to keep track of the number of objects created.
+
+    Methods:
+        __init__(self, id=None): Constructor for Base class.
+            If id is not None, assign the public instance attribute id with this argument value.
+            Otherwise, increment __nb_objects and assign the new value to the public instance attribute id.
+    
     """
     __nb_objects = 0
 
