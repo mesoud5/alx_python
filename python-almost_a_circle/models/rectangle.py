147ada920eb89a33d2344ab5c395f07fa7af2117
mesoud5 <mesoudtemam5@gmail.com>
2024-01-05 12:56:39 -0700

    gv

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -221,12 +221,12 @@ class Rectangle():
         - *args: No-keyword arguments in the order: id, width, height, x, y.
         """
         if len(args) >= 1:
-            self.id = args[0]
+            self.id = args[1]
         if len(args) >= 2:
-            self.width = args[1]
+            self.width = args[2]
         if len(args) >= 3:
-            self.height = args[2]
+            self.height = args[3]
         if len(args) >= 4:
-            self.x = args[3]
+            self.x = args[4]
         if len(args) >= 5:
-            self.y = args[4]
+            self.y = args[5]
