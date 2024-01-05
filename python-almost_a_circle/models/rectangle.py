5bd5c049ce2240d10b563ae59c3492740584a42a
mesoud5 <mesoudtemam5@gmail.com>
2024-01-05 12:36:25 -0700

    mmmm

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -200,8 +200,10 @@ class Rectangle():
         """
         in this method we will print the rectangle in character #
         """
+        for _ in range(self.__y):
+            print()
         for _ in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         """
