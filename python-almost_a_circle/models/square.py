6d750b127e08f87a8191ef48ae708d1351b83993
mesoud5 <mesoudtemam5@gmail.com>
2024-01-05 13:37:35 -0700

    square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,6 +1,19 @@
+"""
+in this module we will import from models,rectangle class rectangle
+"""
 from models.rectangle import Rectangle
 class square(Rectangle):
+    """
+    in this class we have inherited from class rectangle so 
+    we have everything class rectangle have
+    """
     def __init__(self, id, x, y, size):
+        """
+        this is a constructer method
+        """
         super().__init__(id, x, y, size, size)
     def __str__(self):
+        """
+        this is the str method
+        """
         return f"[square] ({id(self)}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
