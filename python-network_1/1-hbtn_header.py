6ea420d9e615ace81632987ff4c17ef6640c597d
mesoud5 <mesoudtemam5@gmail.com>
2024-01-06 06:58:01 -0700

    task 1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -14,7 +14,7 @@ if response.status_code == 200:
     if request_id:
         print(request_id)
     else:
-        print("X-Request-Id header not present in the response.")
+        print()
 
 else:
     print(f"Failed to make the request. Status code: {response.status_code}")
