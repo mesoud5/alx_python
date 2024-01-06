31ec867d4e38cb9f8405552613c746c2b5b6fd3f
mesoud5 <mesoudtemam5@gmail.com>
2024-01-06 05:10:01 -0700

    f

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,5 +2,5 @@ import requests
 url = 'https://alu-intranet.hbtn.io/status'
 response = requests.get(url)
 print("Body response:")
-print(f"    - type:{type(response.text)}")
-print(f"    - content:{response.text}")
+print(f"    - type: {type(response.text)}")
+print(f"    - content: {response.text}")
