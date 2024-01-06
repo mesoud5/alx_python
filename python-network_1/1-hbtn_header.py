88376ca3418666f987c8aec7944128ebe7691196
mesoud5 <mesoudtemam5@gmail.com>
2024-01-06 06:56:05 -0700

    task 1

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,20 @@
+import requests
+import sys
+
+# Get the URL from the command-line argument
+url = sys.argv[1]
+
+# Send an HTTP request to the provided URL
+response = requests.get(url)
+
+# Check if the request was successful (status code 200)
+if response.status_code == 200:
+    # Retrieve and display X-Request-Id header value
+    request_id = response.headers.get('X-Request-Id')
+    if request_id:
+        print(request_id)
+    else:
+        print("X-Request-Id header not present in the response.")
+
+else:
+    print(f"Failed to make the request. Status code: {response.status_code}")
