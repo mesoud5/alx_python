1b66f7d433d2a172e85271528ee6cdf41af347ea
mesoud5 <mesoudtemam5@gmail.com>
2024-01-06 07:36:05 -0700

    task 3

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -5,7 +5,7 @@ import requests
 import sys
 url = sys.argv[1]
 response = requests.get(url)
-if response.status_code <= 400:
+if response.status_code >= 400:
     print(f"Error code:{response.status_code}")
 else:
     print(response.text)
