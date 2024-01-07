ea2c831b2588680af0c4e8fda833053a63a2a330
mesoud5 <mesoudtemam5@gmail.com>
2024-01-07 03:58:45 -0700

    d

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -1,15 +1,16 @@
 import requests
 import sys
-
+from requests.auth import HTTPBasicAuth
 url = 'https://api.github.com/user'
 github_username = sys.argv[1]
 github_token = sys.argv[2]
-
+auth = HTTPBasicAuth(github_username, github_token)
 headers = {
-    'Authorization': f"Basic {github_username}:{github_token}",
     'Accept': 'application/vnd.github.v3+json'
 }
-response = requests.get(url, headers=headers)
+
+response = requests.get(url, auth=auth, headers=headers)
+
 if response.status_code == 200:
     user_data = response.json()
     print(f"{user_data['id']}")
