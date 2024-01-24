996e3a82736c58598622ff52a317580f911957d3
mesoud5 <mesoudtemam5@gmail.com>
2024-01-24 07:49:31 -0700

    task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,33 +1,31 @@
 # IMPORTING MYSQLDB
 import MySQLdb
+import sys
 
-host = "localhost"
-user = "mesoud"
-password = "aymen123"
-database = "hbtn_0e_0_usa"
+# Extract MySQL credentials from command line arguments
+if len(sys.argv) != 4:
+    print("Usage: python script.py <username> <password> <database>")
+    sys.exit(1)
+
+username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
 
 # Connect to MySQL server and specify the database
 connection = MySQLdb.connect(
-    host=host,
-    user=user,
+    host="localhost",
+    user=username,
     passwd=password,
     db=database,
 )
+
 cursor = connection.cursor()
 
-   
-use = "USE hbtn_0e_0_usa"
-cursor.execute(use)
-connection.commit()
 # Select all states and display the results
 all_state = "SELECT id, name FROM states ORDER BY states.id"
 cursor.execute(all_state)
 states = cursor.fetchall()
 
 for state in states:
-        print(state)
+    print(state)
 
-cursor.close()  
+cursor.close()
 connection.close()
-
-
