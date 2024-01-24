4a1209512035b35112db239db3e33de1e450a589
mesoud5 <mesoudtemam5@gmail.com>
2024-01-24 07:43:49 -0700

    TASK 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,42 +7,34 @@ password = "aymen123"
 database = "hbtn_0e_0_usa"
 
 # Connect to MySQL server and specify the database
-with MySQLdb.connect(
+connection = MySQLdb.connect(
     host=host,
     user=user,
     passwd=password,
     db=database,
-) as connection:
-    cursor = connection.cursor()
+)
+cursor = connection.cursor()
 
-    # Create the database if it doesn't exist
-    new_database = "CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa"
-    cursor.execute(new_database)
-    connection.commit()
-    use = "USE hbtn_0e_0_usa"
-    cursor.execute(use)
-    connection.commit()
-    new_table = """CREATE TABLE IF NOT EXISTS states ( 
-    id INT NOT NULL AUTO_INCREMENT, 
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
-    );"""
-    cursor.execute(new_table)
-    connection.commit()
-    insert = "INSERT INTO states (name) VALUES (%s), (%s)"
-    values = ("California", "Arizona")
-    cursor.execute(insert, values)
-    connection.commit()
+   
+use = "USE hbtn_0e_0_usa"
+cursor.execute(use)
+connection.commit()
+# Execute the DELETE statement with a WHERE clause
+delete_query = "DELETE FROM states WHERE id IN (%s, %s)"
+condition_value = (3, 4)
+cursor.execute(delete_query, condition_value)
 
-    # Select all states and display the results
-    all_state = "SELECT id, name FROM states ORDER BY states.id"
-    cursor.execute(all_state)
-    states = cursor.fetchall()
+# Commit the changes
+connection.commit()
+# Select all states and display the results
+all_state = "SELECT id, name FROM states ORDER BY states.id"
+cursor.execute(all_state)
+states = cursor.fetchall()
 
-    for state in states:
+for state in states:
         print(state)
 
-    cursor.close()  
-    connection.close()
+cursor.close()  
+connection.close()
 
 
