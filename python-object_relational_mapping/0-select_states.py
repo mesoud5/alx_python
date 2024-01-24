89ca6cb8e71217299da6a469b499f31f9496c975
mesoud5 <mesoudtemam5@gmail.com>
2024-01-24 05:43:30 -0700

    fgh

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -14,6 +14,9 @@ connection = MySQLdb.connect(
 )
 
 cursor = connection.cursor()
+new_database = "CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa"
+cursor.execute(new_database)
+connection.commit()
 all_state = "SELECT id, name FROM states ORDER BY states.id"
 cursor.execute(all_state)
 states = cursor.fetchall()
