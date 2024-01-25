99bdf9fcd1c1b277f75016d1b0aa224698dcf2a9
mesoud5 <mesoudtemam5@gmail.com>
2024-01-25 00:51:40 -0700

    task 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,31 @@
+# IMPORTING MYSQLDB
+import MySQLdb
+import sys
+
+# Extract MySQL credentials from command line arguments
+if len(sys.argv) != 5:
+    print("Usage: python script.py <username> <password> <database>")
+    sys.exit(1)
+
+username, password, database, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+
+# Connect to MySQL server and specify the database
+connection = MySQLdb.connect(
+    host="localhost",
+    user=username,
+    passwd=password,
+    db=database
+)
+
+cursor = connection.cursor()
+
+# Select all states and display the results
+all_state = "SELECT id, name FROM states WHERE name = '{}' COLLATE utf8mb4_bin ORDER BY states.id ASC".format(state_name)
+cursor.execute(all_state)
+states = cursor.fetchall()
+
+for state in states:
+    print(state)
+
+cursor.close()
+connection.close()
