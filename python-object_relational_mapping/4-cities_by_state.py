2fc1e110974075d88c52bb187c1709b54141ec50
mesoud5 <mesoudtemam5@gmail.com>
2024-01-25 02:17:17 -0700

    nh

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -20,7 +20,7 @@ connection = MySQLdb.connect(
 cursor = connection.cursor()
 
 # Select all states and display the results
-all_cities = "SELECT id, name, state_name FROM cities ORDER BY cities.id ASC"
+all_cities = "SELECT id, name, FROM cities ORDER BY cities.id ASC"
 cursor.execute(all_cities)
 cities = cursor.fetchall()
 
