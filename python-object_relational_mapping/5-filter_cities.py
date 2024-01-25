f1a5e54a9510a6d1d0a705c01b446ab4f2fa2177
mesoud5 <mesoudtemam5@gmail.com>
2024-01-25 05:22:45 -0700

    task 5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,24 +1,51 @@
-# IMPORTING MYSQLDB
+#!/usr/bin/python3
+"""
+Script that lists all cities from the database hbtn_0e_4_usa
+"""
+
 import MySQLdb
 import sys
 
-username, password, database, states_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+if __name__ == "__main__":
+    # Check if all arguments are provided
+    if len(sys.argv) != 5:
+        print("Usage: {} <username> <password> <database> <state>".format(sys.argv[0]))
+        sys.exit(1)
 
-connection = MySQLdb.connect(
-    host="localhost",
-    user=username,
-    passwd=password,
-    db=database,
-)
+    # Retrieve command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state = sys.argv[4]
 
-cursor = connection.cursor()
-cursor.execute("USE hbtn_0e_4_usa")
-cities_in_state = "SELECT cities.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC"
-cursor.execute(cities_in_state)
-states = cursor.fetchall()
+    # Connect to MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
 
-for state in states:
-    print(state)
+    # Create a MySQL cursor
+    cursor = db.cursor()
 
-cursor.close()
-connection.close()
+    # Execute SQL query
+    cursor.execute(
+        """
+        SELECT cities.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
+        """,
+        (state,)
+    )
+
+    # Fetch all rows
+    rows = cursor.fetchall()
+
+    # Display results
+    if rows:
+        cities = [row[0] for row in rows]
+        print(", ".join(cities))
+    else:
+        print()
+
+    # Close cursor and database connection
+    cursor.close()
+    db.close()
