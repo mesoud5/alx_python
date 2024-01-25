e727c02c3135a019e01dc22ce65930e4f135e424
mesoud5 <mesoudtemam5@gmail.com>
2024-01-25 05:11:23 -0700

    task 5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -13,7 +13,7 @@ connection = MySQLdb.connect(
 
 cursor = connection.cursor()
 cursor.execute("USE hbtn_0e_4_usa")
-cities_in_state = "SELECT name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC"
+cities_in_state = "SELECT cities.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC"
 cursor.execute(cities_in_state)
 states = cursor.fetchall()
 
