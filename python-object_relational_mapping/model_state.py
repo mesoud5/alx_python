dfd8734c4362eb0f4d913bd00952b8f431f2086f
mesoud5 <mesoudtemam5@gmail.com>
2024-01-26 03:38:11 -0700

    task 11

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -5,41 +5,32 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import sessionmaker
 
-
+# MySQL database connection parameters
 username = "mesoud"
 password = "aymen123"
 database = "hbtn_0e_6_usa"
 
+# Create the connection path
 path = "mysql+mysqldb://{}:{}@localhost/{}".format(username, password, database)
-database = create_engine(path)
-connection = database.connect()
+engine = create_engine(path)
+connection = engine.connect()
 
+# Create a base class for declarative models
 Base = declarative_base()
-Session = sessionmaker(bind=database) 
+
 class State(Base):
     """Class representing the 'states' table in the database.
 
-        Attributes:
+    Attributes:
         id (int): Auto-generated unique integer, primary key.
         name (str): String with a maximum of 128 characters, cannot be null.
     """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
-    name =  Column(String(128), nullable=False)
+    name = Column(String(128), nullable=False)
+
 # Create the 'states' table in the database
-Base.metadata.create_all(bind=database)
-# Creating a session
-#session = Session()
+Base.metadata.create_all(bind=engine)
 
-# Adding a new state to the database
-#new_state = State(name='New York')
-#session.add(new_state)
-#session.commit()
-
-# Querying the states from the database
-#states = session.query(State).all()
-#for state in states:
-    #print(state.id, state.name)
-
-# Closing the session
-#session.close()
+# Close the connection
+connection.close()
