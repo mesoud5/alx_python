964146ebbf00872758d2f60f1dccd773d4db03c7
mesoud5 <mesoudtemam5@gmail.com>
2024-01-26 05:25:03 -0700

    task 2

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,8 +1,13 @@
+"""
+in this module we will do some sql alchemy projects
+"""
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
-
+"""
+this is instance of base class  holding declarative base function
+"""
 class State(Base):
     """Class representing the 'states' table in the database.
 
@@ -11,5 +16,8 @@ class State(Base):
         name (str): String with a maximum of 128 characters, cannot be null.
     """
     __tablename__ = 'states'
+    """
+    creating table name
+    """
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
