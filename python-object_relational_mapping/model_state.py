2a7f918a25e75f6a58853e4ffbd09f8d4f3c4230
mesoud5 <mesoudtemam5@gmail.com>
2024-01-26 03:53:30 -0700

    task 1

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,21 +1,6 @@
-"""This module defines a SQLAlchemy State class representing a 'states' table in the database."""
-
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import sessionmaker
 
-# MySQL database connection parameters
-username = "mesoud"
-password = "aymen123"
-database = "hbtn_0e_6_usa"
-
-# Create the connection path
-path = "mysql+mysqldb://{}:{}@localhost/{}".format(username, password, database)
-engine = create_engine(path)
-connection = engine.connect()
-
-# Create a base class for declarative models
 Base = declarative_base()
 
 class State(Base):
@@ -28,9 +13,3 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-
-# Create the 'states' table in the database
-Base.metadata.create_all(bind=engine)
-
-# Close the connection
-connection.close()
