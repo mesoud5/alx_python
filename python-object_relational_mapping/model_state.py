b9a2e1600a46279c3c8a6b9c9b34dc3b77524f78
mesoud5 <mesoudtemam5@gmail.com>
2024-01-26 01:24:02 -0700

    task 2

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -15,11 +15,16 @@ connection = database.connect()
 Base = declarative_base()
 Session = sessionmaker(bind=database) 
 class State(Base):
-    """in this class we will create states table using sql alchemy"""
+    """Class representing the 'states' table in the database.
+
+        Attributes:
+        id (int): Auto-generated unique integer, primary key.
+        name (str): String with a maximum of 128 characters, cannot be null.
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name =  Column(String(128), nullable=False)
-Base.metadata.create_all(bind=database)
+    Base.metadata.create_all(bind=database)
 # Creating a session
 #session = Session()
 
