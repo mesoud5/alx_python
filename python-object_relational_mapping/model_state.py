c8350f70b17ce79bfcee6f7b53aa708dc206e648
mesoud5 <mesoudtemam5@gmail.com>
2024-01-26 01:25:17 -0700

    task 1

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,3 +1,6 @@
+"""
+in this code we will use sql alcehmy
+"""
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
