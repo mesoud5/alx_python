3a955d86f49389ea009c51c6707add9e6872609a
mesoud5 <mesoudtemam5@gmail.com>
2024-01-29 12:53:20 -0700

    task 2

---
+++ python-web_framework/2-c_route.py
@@ -0,0 +1,22 @@
+"""
+this module is for flask project
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb')
+def two():
+    return 'HBNB'
+
+@app.route('/c/<text>')
+def hbnb(text):
+    return f'C {text}'
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port="5000")
