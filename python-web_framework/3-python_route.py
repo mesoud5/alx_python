d3d1532e3997f42800efd19f51389d01d108f122
mesoud5 <mesoudtemam5@gmail.com>
2024-01-29 13:48:08 -0700

    task 3

--- python-web_framework/3-python_route.py
+++ python-web_framework/3-python_route.py
@@ -24,5 +24,6 @@ def Python(text='is cool'):
     formated_text = text.replace('_', ' ')
     return f'Python {formated_text}'
 
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port="5000")
