8cff11fcd49dd3635cdd3bbc1e582a30de589a98
mesoud5 <mesoudtemam5@gmail.com>
2024-02-27 01:32:34 -0700

    1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,48 +1,47 @@
 #!/usr/bin/python3
-import csv
-import requests
+
 import sys
+import requests
+import csv
 
+def get_TODOlist(employee_id):
+    endpoint = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    
+    response = requests.get(endpoint)
 
-def export_to_csv(employee_id):
-    # Define API endpoints
-    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print("Error: Failed to make successful request")
+        return None
 
-    # Fetch user information
-    user_response = requests.get(user_url)
-    if user_response.status_code != 200:
-        print("Error fetching user information")
-        return
-    user_data = user_response.json()
-    user_id = user_data['id']
-    username = user_data['username']
-
-    # Fetch TODO list
-    todos_response = requests.get(todos_url)
-    if todos_response.status_code != 200:
-        print("Error fetching TODO list")
-        return
-    todos_data = todos_response.json()
-
-    # Write data to CSV file
-    csv_filename = f"{user_id}.csv"
-    with open(csv_filename, 'w', newline='') as csvfile:
+def export_to_CSV(employee_id, employee_name, tasks):
+    filename = f"{employee_id}.csv"
+    with open(filename, mode='w', newline='') as csvfile:
         fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
         writer.writeheader()
-        for todo in todos_data:
-            writer.writerow({'USER_ID': user_id,
-                             'USERNAME': username,
-                             'TASK_COMPLETED_STATUS': str(todo['completed']),
-                             'TASK_TITLE': todo['title']})
+        for task in tasks:
+            writer.writerow({
+                'USER_ID': employee_id,
+                'USERNAME': employee_name,
+                'TASK_COMPLETED_STATUS': str(task['completed']),
+                'TASK_TITLE': task['title']
+            })
+    print(f"Data exported to {filename}")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python3 script.py <employee_id>")
+        print("Usage: python script_name.py employee_id")
         sys.exit(1)
 
-    employee_id = int(sys.argv[1])
-    export_to_csv(employee_id)
+    employee_id = sys.argv[1]
 
+    tasks = get_TODOlist(employee_id)
+
+    if tasks:
+        employee_name = tasks[0]['userId']
+        export_to_CSV(employee_id, employee_name, tasks)
+    else:
+        print("Error: No data retrieved from the URL")
