ba77cb8c0e02424bd9378932e7acb11282d3feda
mesoud5 <mesoudtemam5@gmail.com>
2024-02-28 05:41:26 -0700

    task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,42 +1,18 @@
 #!/usr/bin/python3
-
-import requests
+"""script using this REST API, for a given employee ID,
+returns information about his/her TODO list progress."""
+import requests as r
 import sys
 
-def get_todo_progress(employee_id):
-    # Define API endpoints
-    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
-
-    # Fetch user information
-    user_response = requests.get(user_url)
-    if user_response.status_code != 200:
-        print("Error fetching user information")
-        return
-    user_data = user_response.json()
-    employee_name = user_data['name']
-
-    # Fetch TODO list
-    todos_response = requests.get(todos_url)
-    if todos_response.status_code != 200:
-        print("Error fetching TODO list")
-        return
-    todos_data = todos_response.json()
-
-    # Calculate progress
-    total_tasks = len(todos_data)
-    done_tasks = sum(1 for todo in todos_data if todo['completed'])
-
-    # Print progress
-    print(f"Employee {employee_name} is done with tasks({done_tasks}/{total_tasks}):")
-    for todo in todos_data:
-        if todo['completed']:
-            print(f"\t {todo['title']}")  # Adjusted indentation
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 script.py <employee_id>")
-        sys.exit(1)
-
-    employee_id = int(sys.argv[1])
-    get_todo_progress(employee_id)
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/'
+    usr_id = r.get(url + 'users/{}'.format(sys.argv[1])).json()
+    to_do = r.get(url + 'todos', params={'userId': sys.argv[1]}).json()
+#    print(to_do)
+    completed = [title.get("title") for title in to_do if
+                 title.get('completed') is True]
+    print(completed)
+    print("Employee {} is done with tasks({}/{}):".format(usr_id.get("name"),
+                                                          len(completed),
+                                                          len(to_do)))
+    [print("\t {}".format(title)) for title in completed]
